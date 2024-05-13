e136abd66af72d27fda5f3607eaf5c0c477a10ef
FabianChSeekop <fabian_chapero@seekop.com>
2024-05-12 20:10:48 -0600

    Se cambiaron los estilos generales, estilos de los card y se agrego el footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { AppNavBarComponent } from "./components/app-nav-bar/app-nav-bar.compone
 import { SliderComponent } from './components/slider/slider.component';
 import { GridCardsComponent } from './components/grid-cards/grid-cards.component';
 import { FloatButtonComponent } from './components/float-button/float-button.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, AppNavBarComponent,SliderComponent,GridCardsComponent,FloatButtonComponent]
+    imports: [CommonModule, RouterOutlet, AppNavBarComponent,SliderComponent,GridCardsComponent,FloatButtonComponent,FooterComponent]
 })
 export class AppComponent {
-  title = 'online-store';
+  title = 'YanJan';
 }
